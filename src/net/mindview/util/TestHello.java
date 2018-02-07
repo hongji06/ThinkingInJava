5efd28e8d56928409f2142f10f47f86f750e8c1e
hongji06 <407324045@qq.com>
2018-02-07 11:23:00 +0800

    add test file

---
+++ src/net/mindview/util/TestHello.java
@@ -0,0 +1,19 @@
+/**   
+ * @{#} TestHello.java by Hongji.Xu 2018年2月7日 上午11:21:57 
+ * @Title:  TestHello.java   
+ * @Package net.mindview.util   
+ * @Description:
+ * @author: <a href="https://github.com/hongji06">Hongji.Xu</a>   
+ * @version 1.0  
+ */   
+package net.mindview.util;
+
+  
+public class TestHello {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("hi");
+    }
+
+}
