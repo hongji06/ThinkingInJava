2e1a9685cb25a118e4fa795156729eeabc4140df
Administrator <Administrator@75KDKCLUCIYWZUC>
2018-02-12 22:25:55 +0800

    add wait()/notify() method

--- src/itcast/thread/Thread28.java
+++ src/itcast/thread/Thread28.java
@@ -1,3 +1,6 @@
+/**
+ * 练习notify()与wait()的用法，要使用LockObject.wait()和LockObject.notify()/notifyAll()来控制线程间通信
+ */
 package itcast.thread;
 
 import java.util.concurrent.TimeUnit;
@@ -12,131 +15,214 @@ import java.util.concurrent.locks.ReentrantLock;
  * @date: 2018年2月12日 下午2:18:56
  *
  */
+//锁定对象
 class Bussiness {
+	private boolean flag = false;
 
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+}
+
+//子线程
+class SubThread implements Runnable {
+	private Bussiness bussiness;
+
+	public SubThread(Bussiness bussiness) {
+		this.bussiness = bussiness;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 50; i++) {
+			synchronized (bussiness) {
+				while (bussiness.isFlag()) {
+					try {
+						bussiness.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println(Thread.currentThread().getName() + " sub thread of " + i);
+				try {
+					TimeUnit.MILLISECONDS.sleep(20);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				bussiness.setFlag(true);
+				bussiness.notify();
+			}
+		}
+	}
+}
+//主线程
+class MainThread implements Runnable {
+	private Bussiness bussiness;
+
+	public MainThread(Bussiness bussiness) {
+		this.bussiness = bussiness;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 50; i++) {
+			synchronized (bussiness) {
+				while (!bussiness.isFlag()) {
+					try {
+						bussiness.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+				System.out.println(Thread.currentThread().getName() + " main thread of " + i);
+				try {
+					TimeUnit.MILLISECONDS.sleep(20);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				bussiness.setFlag(false);
+				bussiness.notify();
+			}
+		}
+	}
 }
 
 public class Thread28 {
-    private boolean flag = true;
+	private static Bussiness lock = new Bussiness();
+	private boolean flag = false;
 
-    private Bussiness lock = new Bussiness();
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        new Thread28().init();
-        // new Init().init();
-    }
+		new Thread(new SubThread(lock)) {
+		}.start();
 
-    public void init() {
+		new Thread(new MainThread(lock)) {
 
-        new Thread() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    for (int i = 0; i < 50; i++) {
+		}.start();
 
-                        while (flag) {
-                            try {
-                                wait();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        System.out.println(Thread.currentThread().getName() + " sub thread");
-                        try {
-                            TimeUnit.MILLISECONDS.sleep(20);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        flag = true;
-                        notify();
-                    }
-                }
-            }
-        }.start();
+		// new Thread28().init();
+		// new Init().init();
+	}
 
-        new Thread() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    for (int i = 0; i < 50; i++) {
-                        while (!flag) {
-                            try {
-                                wait();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
+	// solution 1
+	public void init() {
+		new Thread() {
+			@Override
+			public void run() {
 
-                        System.out.println(Thread.currentThread().getName() + " main thread");
-                        try {
-                            TimeUnit.MILLISECONDS.sleep(20);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        flag = false;
-                        notify();
-                    }
-                }
-            }
-        }.start();
+				for (int i = 0; i < 50; i++) {
+					synchronized (lock) {
+						while (flag) {
+							try {
+								lock.wait();
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+						System.out.println(Thread.currentThread().getName() + " sub thread");
+						try {
+							TimeUnit.MILLISECONDS.sleep(20);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						flag = true;
+						lock.notify();
+					}
+				}
+			}
+		}.start();
 
-    }
+		new Thread() {
+			@Override
+			public void run() {
+
+				for (int i = 0; i < 50; i++) {
+					synchronized (lock) {
+						while (!flag) {
+							try {
+								lock.wait();
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+
+						System.out.println(Thread.currentThread().getName() + " main thread");
+						try {
+							TimeUnit.MILLISECONDS.sleep(20);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						flag = false;
+						lock.notify();
+					}
+				}
+			}
+		}.start();
+
+	}
 }
 
+// solution 2
 class Init {
-    private volatile boolean flag = true;// 是否主線程
-    private Lock lock = new ReentrantLock();
-    private Condition condition = lock.newCondition();
+	private volatile boolean flag = true;// 是否主線程
+	private Lock lock = new ReentrantLock();
+	private Condition condition = lock.newCondition();
 
-    public void init() {
-        // 子線程
-        new Thread() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 50; i++) {
-                    lock.lock();
-                    try {
-                        while (flag) {
-                            condition.await();
-                        }
-                        System.out.println("sub thread: exec 10");
-                        TimeUnit.MILLISECONDS.sleep(10);
-                        flag = true;
+	public void init() {
+		// 子線程
+		new Thread() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 50; i++) {
+					lock.lock();
+					try {
+						while (flag) {
+							condition.await();
+						}
+						System.out.println("sub thread: exec 10");
+						TimeUnit.MILLISECONDS.sleep(10);
+						flag = true;
 
-                        condition.signalAll();
+						condition.signalAll();
 
-                    } catch (InterruptedException e) {
-                        System.out.println("sub interrupted");
-                    } finally {
-                        lock.unlock();
-                    }
-                }
-            }
-        }.start();
+					} catch (InterruptedException e) {
+						System.out.println("sub interrupted");
+					} finally {
+						lock.unlock();
+					}
+				}
+			}
+		}.start();
 
-        // 主線程
-        new Thread() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 50; i++) {
-                    lock.lock();
-                    try {
-                        while (!flag) {
-                            condition.await();
-                        }
-                        System.out.println("main thread exec 100");
-                        TimeUnit.MILLISECONDS.sleep(10);
-                        flag = false;
-                        condition.signalAll();
+		// 主線程
+		new Thread() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 50; i++) {
+					lock.lock();
+					try {
+						while (!flag) {
+							condition.await();
+						}
+						System.out.println("main thread exec 100");
+						TimeUnit.MILLISECONDS.sleep(10);
+						flag = false;
+						condition.signalAll();
 
-                    } catch (InterruptedException e) {
-                        System.out.println("sub interrupted");
-                    } finally {
-                        lock.unlock();
-                    }
-                }
-            }
-        }.start();
+					} catch (InterruptedException e) {
+						System.out.println("sub interrupted");
+					} finally {
+						lock.unlock();
+					}
+				}
+			}
+		}.start();
 
-    }
+	}
 }
